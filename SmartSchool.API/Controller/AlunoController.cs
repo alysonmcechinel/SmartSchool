d01d79069619da55f65ba9f3898e96a44d539f84
Alyson <Alyson@DESKTOP-ABG2VJ8>
2021-08-01 18:35:29 -0300

    Add project files.

---
+++ SmartSchool.API/Controller/AlunoController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.API.Controller
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class AlunoController : ControllerBase
+    {
+        public List<Aluno> Alunos = new List<Aluno>()
+        {
+            new Aluno()
+            {
+                Id = 1,
+                Nome = "Marcos",
+                Sobrenome = "Almeida",
+                Telefone = "34631176"
+            }
+        };
+
+        public AlunoController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("Aluno não foi encontrado");
+            return Ok(aluno);
+        }
+
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome));
+            if (aluno == null) return BadRequest("Aluno não foi encontrado");
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPut]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPatch]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SmartSchool.API/Controller/ProfessorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.API.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfessorController : ControllerBase
+    {
+
+        public ProfessorController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Teste");
+        }
+    }
+}
